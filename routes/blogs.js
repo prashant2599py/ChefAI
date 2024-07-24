f66e3ff5ab4b9722e4f7fc69a81a2d5a2140f906
Prashant Lodhi <prashantrajput2517@gmail.com>
2024-07-24 16:12:18 +0530

    Revert "This reverts commit b5902c59a99fbef2ae5500dd566558528750170c."
    This reverts commit fc974ecaa3d1dd7eb4de2d6c7eee3b68cf88e7ff.

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,15 +1,40 @@
 const { Router } = require("express");
 const router = Router();
+const multer = require('multer');
+const path = require('path');
 
-// const { Blogs } = require("../models/blogs");
-// const { checkForAuthenticationCookie } = require("../middlewares/user");
+const Blog = require('../models/blogs');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads`));
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName)
+    }
+  })
+
+  const upload = multer({ storage: storage })
 
 
-router.get("/add-new" , (req, res) => {
-    return res.render("/blogs", {
+
+router.get("/add-new", (req, res) => {
+    return res.render("addBlog", {
         user: req.user,
     })
 })
 
+
+router.post("/blogs", upload.single("coverImage"),async (req, res)=> {
+    const { title, body } = req.body;
+    const blog = await Blog.create({
+        body,
+        title,
+        createdBy : req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`,
+    })
+    return res.redirect(`/user/blogs/`)
+})
 
 module.exports = router;
